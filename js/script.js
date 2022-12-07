72c57fd009ac390ef10313614d98160395ebdcfe
ssandoval3 <ssandoval3@hawk.iit.edu>
2022-12-06 23:30:51 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,33 +3,46 @@ function init(){
 	var el = document.getElementById('canvas');
 	var myLocation = new google.maps.LatLng(41.929637, -87.738234);
 	var mapOptions = {
-		center: myLocation,
-		zoom: 17,
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.BOTTOM_CENTER
+	  center: myLocation,
+	  zoom: 17,
+	  mapTypeId: google.maps.MapTypeId.SATELLITE,
+	  mapTypeControlOptions: {
+		  position: google.maps.ControlPosition.BOTTOM_CENTER
 		}
 	};
 
 	var myMap = new google.maps.Map(el, mapOptions);
 
 	var marker = new google.maps.Marker({
-		position: myLocation,
-		map: myMap,
-		animation: google.maps.Animation.DROP,
+	  position: myLocation,
+	  map: myMap,
+	  animation: google.maps.Animation.DROP,
 	});
 
-	var contentString = '<h1>IIT Perlstein Hall</h1><p>This is Kelvyn Park, my neighborhood park. This is where I play a lot of soccer with my friends and made a lot of memories with my family.</p>';
+	var contentString1 = '<h1>Kelvyn Park</h1><p>This is Kelvyn Park, my neighborhood park. This is where I play a lot of soccer with my friends and made a lot of memories with my family.</p>';
 
 	var infowindow = new google.maps.InfoWindow({
       content: contentString
   	});
 
 	google.maps.event.addListener(marker, 'mouseover', function() {
-    	infowindow.open(myMap, marker);
+      infowindow.open(myMap, marker);
   	});
+	
+	const BusRoute = [
+      {lat: 41.924183, lng: -87.736900},
+      {lat:41.925168, lng: -87.652820},
+    ];
+	
+    const trainRoute = new google.maps.Polyline({
+      path: trainRouteCoordinates,
+      geodesic: true,
+      strokeColor: "blue",
+      strokeOpacity: 1.0,
+      strokeWeight: 2,
+    });
 
-
+    trainRoute.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', init);
