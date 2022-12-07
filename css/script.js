156d7c93339da429b0226dd8b70566752803acc6
ssandoval3 <ssandoval3@hawk.iit.edu>
2022-12-06 21:24:17 -0600

    fixed image bug

--- css/script.js
+++ css/script.js
@@ -28,8 +28,6 @@ function init(){
 	google.maps.event.addListener(marker, 'mouseover', function() {
     	infowindow.open(myMap, marker);
   	});
-
-
 }
 
 google.maps.event.addDomListener(window, 'load', init);
